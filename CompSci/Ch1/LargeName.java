81bca3bc9b7158a3716a7e24bb91722d4c40a795
Isaac Blandin Mac <myung0966@gmail.com>
2018-08-27 21:07:03 -0500

    created a class which prints my name in large text

---
+++ CompSci/Ch1/LargeName.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class LargeName here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LargeName
+{
+    public static void main(){
+        System.out.println("***** *****  ***   ***  *****");
+        System.out.println("  *   *     *   * *   * *");
+        System.out.println("  *   ***** ***** ***** *");
+        System.out.println("  *       * *   * *   * *");
+        System.out.println("***** ***** *   * *   * *****");
+    }
+}
