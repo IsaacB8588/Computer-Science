f610e7c1137a58551059299ca4e7e7658ada5cf9
Isaac Blandin Mac <myung0966@gmail.com>
2018-08-27 21:06:48 -0500

    created a class which prints a greeting

---
+++ CompSci/Ch1/Greeting.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Greeting here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Greeting
+{
+    public static void main() {
+        System.out.println("ni hao");
+    }
+}
