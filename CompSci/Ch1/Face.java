1849a85a62af5db421586c57e5b09bf40ce7c93c
Isaac Blandin Mac <myung0966@gmail.com>
2018-08-27 21:06:36 -0500

    created class which prints a face

---
+++ CompSci/Ch1/Face.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Face here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Face
+{
+  public static void main(){
+      System.out.println("|||||||");
+      System.out.println("+''''''+");
+      System.out.println("| o o |");
+      System.out.println("|  L  |");
+      System.out.println("|  __ |");
+      System.out.println("+_____+");
+    }
+}
