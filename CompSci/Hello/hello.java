5d03d73e91b6a42d918211805b3e0d57bbfada5f
Isaac Blandin Mac <myung0966@gmail.com>
2018-08-27 20:26:39 -0500

    Added Project

---
+++ CompSci/Hello/hello.java
@@ -0,0 +1,14 @@
+
+/**
+ * Prints "hello world"
+ * 
+ * @author Isaac Blandin 
+ * @version 8/24/18
+ */
+public class hello
+{
+    public static void main()
+    {
+        System.out.println("hello world");
+    }
+}
